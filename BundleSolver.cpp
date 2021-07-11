76302ac49d0c9d0ece9bcb062c4f053f9e4a44d4
Antonio Frangioni <frangio@di.unipi.it>
2021-07-11 14:46:49 +0200

    added makefile-s

--- BundleSolver.cpp
+++ BundleSolver.cpp
@@ -78,7 +78,7 @@
 /*--------------------------------------------------------------------------*/
 
 #ifndef NDEBUG
- #define CHECK_DS 15
+ #define CHECK_DS 0
  /* Perform long and costly checks on the data structures, coded bit-wise:
   *
   * - CHECK_DS & 1 == checks the data structures representing the bundle and
