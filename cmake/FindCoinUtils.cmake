862b2114d7798db89ced5639ba601c5334daab36
dmeoli <donato.meoli.95@gmail.com>
2025-08-02 16:11:20 +0200

    updated cmake file

--- cmake/FindCoinUtils.cmake
+++ cmake/FindCoinUtils.cmake
@@ -38,6 +38,21 @@ include(FindPackageHandleStandardArgs)
 # ----- Requirements -------------------------------------------------------- #
 find_package(BZip2 REQUIRED QUIET)
 
+# Conda coin-or-utils package links MKL BLAS library
+# https://github.com/conda-forge/coin-or-utils-feedstock/blob/main/recipe/build.sh#L12
+if (WIN32 AND DEFINED ENV{LIBRARY_PREFIX})
+
+    find_library(MKL_RT_LIB
+                 NAMES mkl_rt
+                 PATHS $ENV{LIBRARY_LIB}
+                 NO_DEFAULT_PATH
+                 DOC "mkl_rt library.")
+
+    if (MKL_RT_LIB)
+        set(BLAS_LIBRARIES ${MKL_RT_LIB})
+    endif ()
+endif ()
+
 # Check if already in cache
 if (CoinUtils_INCLUDE_DIR AND CoinUtils_LIBRARY)
     set(CoinUtils_FOUND TRUE)
@@ -96,7 +111,7 @@ if (CoinUtils_FOUND)
                 Coin::CoinUtils PROPERTIES
                 IMPORTED_LOCATION ${CoinUtils_LIBRARY}
                 INTERFACE_INCLUDE_DIRECTORIES ${CoinUtils_INCLUDE_DIRS}
-                INTERFACE_LINK_LIBRARIES "BZip2::BZip2")
+                INTERFACE_LINK_LIBRARIES "BZip2::BZip2;${BLAS_LIBRARIES}")
     endif ()
 endif ()
 
