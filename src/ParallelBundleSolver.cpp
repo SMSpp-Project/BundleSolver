41d53c0be2d90984dde25f23d8ca64ce2f142c91
Donato Meoli <donato.meoli.95@gmail.com>
2026-09-20 03:31:16 +0200

    a component answering kLowPrecision does not abort the parallel inner loop, as it does not abort the sequential one
    (cherry picked from commit beb10c9e9f1f47d6fd6916820a4310e6b18683ba)

--- src/ParallelBundleSolver.cpp
+++ src/ParallelBundleSolver.cpp
@@ -238,7 +238,10 @@ BundleSolver::Index ParallelBundleSolver::InnerLoop( bool extrastep )
 	    << it - EvalV.begin() << " has status " << FiStatus[ wFi ] );
 
   // if an unrecoverable error happens, immediately start the ramp-down - - -
-  if( ( FiStatus[ wFi ] <= kUnEval ) || ( FiStatus[ wFi ] >= kError ) ) {
+  // kLowPrecision is not one of them [see BundleSolver::InnerLoop()]
+  if( ( FiStatus[ wFi ] <= kUnEval ) ||
+      ( ( FiStatus[ wFi ] >= kError ) &&
+	( FiStatus[ wFi ] != kLowPrecision ) ) ) {
    BLOG( 1 , std::endl << "            Component " << wFi
 	     << " evaluated: Error, status " << FiStatus[ wFi ] );
    Result = kError;
@@ -369,7 +372,10 @@ BundleSolver::Index ParallelBundleSolver::InnerLoop( bool extrastep )
  	    << it - EvalV.begin() << " has status " << FiStatus[ wFi ] );
 
   // if an unrecoverable error happens, do nothing else - - - - - - - - - - -
-  if( ( FiStatus[ wFi ] <= kUnEval ) || ( FiStatus[ wFi ] >= kError ) ) {
+  // kLowPrecision is not one of them [see BundleSolver::InnerLoop()]
+  if( ( FiStatus[ wFi ] <= kUnEval ) ||
+      ( ( FiStatus[ wFi ] >= kError ) &&
+	( FiStatus[ wFi ] != kLowPrecision ) ) ) {
    if( f_log && ( LogVerb > 0 ) )
     *f_log << std::endl << "            Component " << wFi
 	   << " evaluated: Error, status " << FiStatus[ wFi ];
@@ -554,7 +560,10 @@ BundleSolver::Index ParallelBundleSolver::InnerLoopOrdered( bool extrastep ,
 	    << FiStatus[ wFi ] );
 
   // unrecoverable error: stop, the whole compute() aborts- - - - - - - - - -
-  if( ( FiStatus[ wFi ] <= kUnEval ) || ( FiStatus[ wFi ] >= kError ) ) {
+  // kLowPrecision is not one of them [see BundleSolver::InnerLoop()]
+  if( ( FiStatus[ wFi ] <= kUnEval ) ||
+      ( ( FiStatus[ wFi ] >= kError ) &&
+	( FiStatus[ wFi ] != kLowPrecision ) ) ) {
    BLOG( 1 , std::endl << "            Component " << wFi
 	     << " evaluated: Error, status " << FiStatus[ wFi ] );
    Result = kError;
